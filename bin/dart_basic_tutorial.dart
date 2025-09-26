fa9c36b1a53381cb41d950080c75b8f529ed1caa
silpako <silpako167@gmail.com>
2025-09-26 13:37:44 +0530

    commir

--- bin/dart_basic_tutorial.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_basic_tutorial/dart_basic_tutorial.dart' as dart_basic_tutorial;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_basic_tutorial.calculate()}!');
-}
